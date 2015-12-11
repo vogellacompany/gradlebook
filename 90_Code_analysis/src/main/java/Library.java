ad1b960943d2e23aa0e8ae8a03548d7ab7d7aee0
Simon Scholz <simon.scholz@vogella.com>
2015-12-11 02:35:53 +0100

    Added Code Analysis exercise
    Change-Id: I1bb689aab5f15ff6867fdaae719b9b9334fea185
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ 90_Code_analysis/src/main/java/Library.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Library {
+    public boolean someLibraryMethod() { 
+	String test = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	Integer three = Integer.valueOf(3);
+	three.equals(test); 
+	return true;
+    }
+}
