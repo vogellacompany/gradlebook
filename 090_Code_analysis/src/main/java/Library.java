76990d2f95b80dd2f2bcfd2ff672ec04f4e3418b
Simon Scholz <simon.scholz@vogella.com>
2016-03-21 22:00:23 +0100

    [CodeAnalysis] Added jacoco plugin
    Change-Id: I26aa34faea7da4d0642fc0f047c33421eb448e0e
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- 090_Code_analysis/src/main/java/Library.java
+++ 090_Code_analysis/src/main/java/Library.java
@@ -2,9 +2,15 @@ import java.util.Date;
 
 public class Library {
     public boolean someLibraryMethod() { 
-	String test = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-	Integer three = Integer.valueOf(3);
-	three.equals(test); 
-	return true;
+		String test = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		Integer three = Integer.valueOf(3);
+		three.equals(test); 
+		
+		return true;
+    }
+
+    // the jacoco plugin will find out that this method has not been tested
+    public boolean someOtherUntestedMethod() {
+		return true;
     }
 }
