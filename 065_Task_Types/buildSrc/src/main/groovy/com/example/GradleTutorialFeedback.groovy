30f7b3a9c9d14e82d5010e8c535587b25565eba9
Simon Scholz <simon.scholz@vogella.com>
2016-03-22 18:06:08 +0100

    [TaskTypes] Provide a GradleTutorialFeedback template in buildSrc folder
    Change-Id: I9b3b9676ed040e3e67553fa80ce90d29d5f268af
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ 065_Task_Types/buildSrc/src/main/groovy/com/example/GradleTutorialFeedback.groovy
@@ -0,0 +1,11 @@
+package com.example
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class GradleTutorialFeedback /* extends ? */ {
+	def feedback = "Super Training hier"
+	
+	// implement task action here
+
+}
